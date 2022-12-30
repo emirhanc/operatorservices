7a0044abe248cd72c00c528b6b79d0374309cea6
Emirhan <29688260+emirhanc@users.noreply.github.com>
2022-12-31 01:40:04 +0300

    Remove unnecessary code

--- purchase-order-service/src/main/java/com/operatorservices/purchaseorderservice/PurchaseOrderServiceApplication.java
+++ purchase-order-service/src/main/java/com/operatorservices/purchaseorderservice/PurchaseOrderServiceApplication.java
@@ -3,11 +3,9 @@ package com.operatorservices.purchaseorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
-@EnableCaching
 public class PurchaseOrderServiceApplication {
 
 	public static void main(String[] args) {
